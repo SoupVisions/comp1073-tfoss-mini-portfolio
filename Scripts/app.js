a0d0d52d817d45b6e92fd7d542ed8556756b9fdb
Theodore Foss <fritzfoss@hotmail.com>
2016-02-12 19:07:25 -0500

    inserted header/nav txt using js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,49 +1,50 @@
 "use strict";
 
-// ASEF or IIFE (Immediately Invoked Function Expression) 
-// IIFE is automatically executed - this is the basic structure everytime you start
+// IIFE (Immediately Invoked Function Expression) 
 (function () {
-
+	
+	// test to see if js is working
 	console.log("App started...");
-
-	// named function
-	// in our class, use the named function 
-	/*
-	function replaceFirstParagraph() {
-
-		console.log("inside replaceFirstParagraph function");
-		
-		var firstParagraph;
-
-		firstParagraph = document.getElementById("firstParagraph");
-
-		firstParagraph.innerHTML = "My new paragraph...";
+	
+	// named functions to insert text:
+	
+	/* Header/Nav
+	============== */
+	
+	function insertSiteTitle() {
+		var siteTitle = document.getElementById("site-title");
+		siteTitle.innerHTML = "Theodore F. Foss";
 	}
+	insertSiteTitle();
 	
-	// call replaceFirstParagraph function 
-	replaceFirstParagraph();
-
-	var secondParagraph;
-
-	secondParagraph = document.getElementById("secondParagraph");
-
-	secondParagraph.innerHTML = "Another new paragraph...";
-	*/
-	
-	// anonymous function with an alias (don't use)
-	var replaceFirstParagraph = function() {
-		
-		console.log("inside replaceFirstParagraph function");
-		
-		var firstParagraph;
-
-		firstParagraph = document.getElementById("firstParagraph");
-
-		firstParagraph.innerHTML = "My new paragraph...";
+	function insertMenuItems() {
+		var menuItems = [ 
+						  {item01: document.getElementById("menu-about")},
+						  {item02: document.getElementById("menu-portfolio")},
+						  {item03: document.getElementById("menu-contact")}
+						]; 
+						menuItems[0].item01.innerHTML = "About Me";
+						menuItems[1].item02.innerHTML = "My Projects"
+						menuItems[2].item03.innerHTML = "Contact Me";	
 	}
+	insertMenuItems();
 	
-	replaceFirstParagraph();
+	
+	/* About Me Page
+	================= */
 
+	function insertMissionStatement() {
+		var missionStatementHeading = document.getElementById("mission-statement-heading");
+		missionStatementHeading.innerHTML = "Mission Statement";
+		var missionStatement = document.getElementById("mission-statement-paragraph");
+		missionStatement.innerHTML = "I make you nice website. You pay me.";	
+	}
+	insertMissionStatement();
+	
+	
+	
+	
+	
 })();
 
 
