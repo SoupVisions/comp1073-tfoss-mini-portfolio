a8d2b650a7bbf672a1758865cd63208c376bea80
Theodore Foss <fritzfoss@hotmail.com>
2016-04-21 00:35:24 -0400

    added tsd code hinting for createjs

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,2 +1,5 @@
 
 /// <reference path="jquery/jquery.d.ts" />
+/// <reference path="createjs-lib/createjs-lib.d.ts" />
+/// <reference path="easeljs/easeljs.d.ts" />
+/// <reference path="tweenjs/tweenjs.d.ts" />
