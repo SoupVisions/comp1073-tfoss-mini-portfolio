41d70cbb9cd7ba4b1bcbfb8211c06726fcd43ba5
Theodore Foss <fritzfoss@hotmail.com>
2016-04-21 00:37:47 -0400

    added preloadjs code hinting

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="createjs-lib/createjs-lib.d.ts" />
 /// <reference path="easeljs/easeljs.d.ts" />
 /// <reference path="tweenjs/tweenjs.d.ts" />
+/// <reference path="preloadjs/preloadjs.d.ts" />
