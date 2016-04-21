7017de9a5a9dc6491c15066d42969045c80c4e6b
Theodore Foss <fritzfoss@hotmail.com>
2016-04-21 14:57:30 -0400

    created stage object and ticker for the canvas space

--- Scripts/banner-ad.js
+++ Scripts/banner-ad.js
@@ -5,6 +5,14 @@
 	
 	"use strict";
 	
+	// grab the canvas element and reference it by assigning it a variable
+	var canvas = $("#canvas");
+	// create an object for the stage and reference it by assigning it a variable
+	var stage = new createjs.Stage(canvas);
+	// set up frame rate (create a ticker for how many frames per second)
+	// (ticker is a built-in global object in createjs)
+	// set framerate to 60 frames per second:
+	createjs.Ticker.setFPS(60);
 	
 	
 	
